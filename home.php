4ed27858160b451ee63fc188e887e13ec5f8f876
Nicolas Hamparsomian <n_hampa@hotmail.com>
2017-04-16 15:38:14 -0300

    Get municipio / Load bairro

--- home.php
+++ home.php
@@ -4,8 +4,11 @@
 	$username = "root";
 	$password = "";
   $dbname = "acessibilidade_escolas";
+	$municipio = "";
+	//isset($_POST['dropdown-municipio']) ? $_POST['dropdown-municipio'] : false;
 
 	include("home.html");
+	//$municipio = $_GET['dropdown-municipio'];
 
 	try
 	{
@@ -25,12 +28,37 @@
 			while($row = $result->fetch(PDO::FETCH_ASSOC)) {
 				//echo "Bairro: " . $row["bairro"]. "<br>";
 				?><script>
-					$( ".form-control" )
-						.append( "<option> <?php echo $row["municipio"]; ?> </option>" );
+					$( "#dropdown-municipio" )
+						.append( "<option value='<?php echo $row["municipio"]; ?>'> <?php echo $row["municipio"]; ?> </option>" );
 				</script><?php
 			}
 		} else {
 			echo "0 results";
+		}
+
+		if(!empty($_GET)){ // If you are using same page, then it'll help you to detect ajax request.
+			$municipio =  $_GET['municipio'];
+			echo $municipio;
+			//$a =isset($_GET['municipio'])?$_GET['municipio']:'not yet';
+			//echo $a ;,
+			$sql = "SELECT DISTINCT bairro FROM escola_geral WHERE municipio='$municipio' ORDER BY bairro ASC";
+
+			$result = $conn->prepare($sql);
+	    try { $result->execute();}
+	    catch(PDOException $e){echo $e->getMessage();}
+
+			if ($result->rowCount() > 0) {
+				// output data of each row
+				while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+					//echo "Bairro: " . $row["bairro"]. "<br>";
+					?><script>
+						$( "#dropdown-bairro" )
+							.append( "<option value='<?php echo $row["bairro"]; ?>'> <?php echo $row["bairro"]; ?> </option>" );
+					</script><?php
+				}
+			} else {
+				echo "0 results";
+			}
 		}
 	}
 	catch(PDOException $e)
