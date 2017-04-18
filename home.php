5fee883cd380cd68e2b79d4c4a9dcd7e96ee670b
Nicolas Hamparsomian <n_hampa@hotmail.com>
2017-04-17 23:34:44 -0300

    Code Cleaning

--- home.php
+++ home.php
@@ -5,10 +5,8 @@
 	$password = "";
   $dbname = "acessibilidade_escolas";
 	$municipio = "";
-	//isset($_POST['dropdown-municipio']) ? $_POST['dropdown-municipio'] : false;
 
 	include("home.html");
-	//$municipio = $_GET['dropdown-municipio'];
 
 	try
 	{
@@ -26,7 +24,6 @@
 		if ($result->rowCount() > 0) {
 			// output data of each row
 			while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-				//echo "Bairro: " . $row["bairro"]. "<br>";
 				?><script>
 					$( "#dropdown-municipio" )
 						.append( "<option value='<?php echo $row["municipio"]; ?>'> <?php echo $row["municipio"]; ?> </option>" );
@@ -36,11 +33,9 @@
 			echo "0 results";
 		}
 
-		if(!empty($_GET)){ // If you are using same page, then it'll help you to detect ajax request.
+		if(!empty($_GET['municipio'])){ // If you are using same page, then it'll help you to detect ajax request.
+
 			$municipio =  $_GET['municipio'];
-			echo $municipio;
-			//$a =isset($_GET['municipio'])?$_GET['municipio']:'not yet';
-			//echo $a ;,
 			$sql = "SELECT DISTINCT bairro FROM escola_geral WHERE municipio='$municipio' ORDER BY bairro ASC";
 
 			$result = $conn->prepare($sql);
@@ -50,7 +45,6 @@
 			if ($result->rowCount() > 0) {
 				// output data of each row
 				while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-					//echo "Bairro: " . $row["bairro"]. "<br>";
 					?><script>
 						$( "#dropdown-bairro" )
 							.append( "<option value='<?php echo $row["bairro"]; ?>'> <?php echo $row["bairro"]; ?> </option>" );
