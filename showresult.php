8ead672f747d8a21cb3e368ef11423703afac93c
Nicolas Hamparsomian <n_hampa@hotmail.com>
2017-04-21 01:32:05 -0300

    Show Search Result on jsGrid

--- showresult.php
+++ showresult.php
@@ -3,17 +3,77 @@
 $servername="localhost";
 $username="root";
 $password="";
+$dbname = "acessibilidade_escolas";
 
-//include("home.html");
+include("showresult.html");
 
 try
 {
-   $conn = new PDO("mysql:host=$servername;dbname=acessibilidade_escolas", $username, $password);
-   // set the PDO error mode to exception
-   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   echo "Connected successfully";
-   echo $_GET['municipio'];
-   echo $_GET['bairro'];
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //echo "Connected successfully";
+
+  $municipio=$_GET['municipio'];
+  $bairro=$_GET['bairro'];
+  //echo $municipio;
+  //echo $bairro;
+
+  $sql = "SELECT nome_escola,dependencia_administrativa,bairro,endereco,telefone FROM escola_geral WHERE municipio='$municipio' AND bairro='$bairro' ORDER BY nome_escola ASC";
+
+  $result = $conn->prepare($sql);
+  try { $result->execute();}
+  catch(PDOException $e){echo $e->getMessage();}
+
+  if ($result->rowCount() > 0) {
+    // output data of each row
+    $i=0;
+    while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+      $escola = $row["nome_escola"];
+      $administracao = $row["dependencia_administrativa"];
+      $bairro = $row["bairro"];
+      $endereco = $row["endereco"];
+      $telefone = $row["telefone"];
+      ?>
+      <script>
+        var objPush = {};
+        objPush.Escola = "<?php echo $escola; ?>";
+        objPush.Administração = "<?php echo $administracao; ?>";
+        objPush.Bairro = "<?php echo $bairro; ?>";
+        objPush.Endereco = "<?php echo $endereco; ?>";
+        objPush.Numero = "<?php echo $telefone; ?>";
+        escolas.push(objPush);
+      </script>
+      <?php
+    }
+    ?>
+    <script>
+      $("#jsGrid").jsGrid({
+        align: "center",
+        width: "100%",
+        height: "100%",
+
+        inserting: false,
+        editing: false,
+        sorting: false,
+        paging: false,
+
+        data: escolas,
+
+        fields: [
+          { name: "Escola", type: "text", width: 150 },
+          { name: "Administração", type: "text", width: 60 },
+          { name: "Bairro", type: "text", width: 100 },
+          { name: "Endereco", type: "text", width: 180 },
+          { name: "Numero", type: "text", width:60 },
+          { type: "textarea"}
+        ]
+      });
+    </script>
+    <?php
+  } else {
+    echo "0 results";
+  }
 }
 catch(PDOException $e)
 {
